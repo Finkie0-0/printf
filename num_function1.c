742d3dd2b1858afe5904fcdf2e1d7774406609d0
Finkie0-0 <104798727+Finkie0-0@users.noreply.github.com>
2022-07-13 05:15:29 +0200

    Added u

--- num_function1.c
+++ num_function1.c
@@ -32,3 +32,36 @@ int print_unsigned_int(va_list b)
 	}
 	return (counter);
 }
+
+/**
+ * print_unsigned_decimal - prints out the unsigned decimal
+ * @u: unsigned decimal number to be printed out.
+ *
+ * Return: number of characters printed
+ */
+
+int print_unsigned_decimal(va_list u)
+{
+	unsigned int x, y, z, sum;
+	unsigned int arr[10];
+	int counter;
+
+	x = va_arg(u, unsigned int);
+	y = 1000000000;
+	arr[0] = x / y;
+	for (z = 1; z < 10; z++)
+	{
+		y /= 10;
+		arr[z] = (x / y) % 10;
+	}
+	for (z = 0, sum = 0, counter = 0; z < 10; z++)
+	{
+		sum += arr[z];
+		if (sum || z == 9)
+		{
+			_putchar('0' + arr[z]);
+			counter++;
+		}
+	}
+	return (counter);
+}
