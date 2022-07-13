2002db2f427593d861bb246d4b08ff69691492b7
Finkie0-0 <104798727+Finkie0-0@users.noreply.github.com>
2022-07-13 05:17:01 +0200

    Added the O

--- num_function1.c
+++ num_function1.c
@@ -65,3 +65,36 @@ int print_unsigned_decimal(va_list u)
 	}
 	return (counter);
 }
+
+/**
+ * print_octal- prints octal number
+ * @o: octal to print out
+ *
+ * Return: Number of octal char printed
+ */
+int print_octal(va_list o)
+{
+	unsigned int arr[11];
+	unsigned int x, y, z, sum;
+	int counter;
+
+	z = va_arg(o, unsigned int);
+	y = 1073741824;
+	arr[0] = z / y;
+	for (x = 1; x < 11; x++)
+	{
+		y /= 8;
+		arr[x] = (z / y) % 8;
+	}
+	for (x = 0, sum = 0, counter = 0; x < 11; x++)
+	{
+		sum += arr[x];
+		if (sum || x == 10)
+		{
+			_putchar('0' + arr[x]);
+			counter++;
+		}
+	}
+	return (counter);
+}
+
